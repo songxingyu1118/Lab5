f743b6be928990dfb010bc3ce2ced65468c0f5f3
ArkSchoolAccount <79713632+ArkSchoolAccount@users.noreply.github.com>
2021-02-26 12:50:23 -0500

    Main commit

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("what is your name")
+age = input("what is your age")
+
+if age > 90:
+    print("hi", name, "youre old")
+else:
+    print("hi", name, "youre stastically still probably old")
